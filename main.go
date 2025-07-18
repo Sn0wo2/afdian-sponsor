ba87085336b9913ee1eedec8a8ac969d36c85a0d
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-07-18 17:49:56 +0800

    fix: nolint

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	userID, apiToken, output, page, perPage, avatarSize, margin, avatarsPerRow := getConfig()
 
-	if err := os.WriteFile(output, []byte(generateSVG(querySponsor(userID, apiToken, page, perPage), avatarSize, margin, avatarsPerRow)), 0o644); err != nil {
+	if err := os.WriteFile(output, []byte(generateSVG(querySponsor(userID, apiToken, page, perPage), avatarSize, margin, avatarsPerRow)), 0o644); err != nil { //nolint:gosec
 		panic(err)
 	}
 
