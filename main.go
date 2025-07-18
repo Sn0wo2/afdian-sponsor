d1e492b228d0dd99016e2d77de3b0f8076167633
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-07-18 17:48:16 +0800

    fix: output permission

--- main.go
+++ main.go
@@ -8,10 +8,7 @@ import (
 func main() {
 	userID, apiToken, output, page, perPage, avatarSize, margin, avatarsPerRow := getConfig()
 
-	sponsor := querySponsor(userID, apiToken, page, perPage)
-
-	err := os.WriteFile(output, []byte(generateSVG(sponsor, avatarSize, margin, avatarsPerRow)), 0o600)
-	if err != nil {
+	if err := os.WriteFile(output, []byte(generateSVG(querySponsor(userID, apiToken, page, perPage), avatarSize, margin, avatarsPerRow)), 0o644); err != nil {
 		panic(err)
 	}
 
